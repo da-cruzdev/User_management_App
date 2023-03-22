9e8f900e38fb20285d7116d8e56e987b19150703
Da-cruz <wilfrieddacruz@arolitec.com>
2023-03-22 15:14:00 +0000

    add trim method in customer validator

--- server/validators/customerValidator.js
+++ server/validators/customerValidator.js
@@ -1,15 +1,17 @@
 const Joi = require("joi");
 
 const validation = Joi.object({
-  firstName: Joi.string().min(2).max(30).required(),
-  lastName: Joi.string().min(2).max(30).required(),
+  firstName: Joi.string().min(2).max(30).trim().required(),
+  lastName: Joi.string().min(2).max(30).trim().required(),
   tel: Joi.string()
     .pattern(/^\d{10}$/)
+    .trim()
     .required(),
   email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "fr"] } })
+    .trim()
     .required(),
-  details: Joi.string().max(500),
+  details: Joi.string().max(500).trim(),
 });
 
 module.exports = validation;
